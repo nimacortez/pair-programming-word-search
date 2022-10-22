1cebd3da2e14ea73bcfc8f3cea8a3a5fed2af7bb
Nima Cortez <cortez.nima@gmail.com>
2022-10-22 19:33:11 +0000

    adding wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
-
-module.exports = wordSearch
+      if (l.includes(word)) return true;
+    };
+     const vertical = letters[0].map((_, colIndex) => letters.map((row) => row[colIndex]));
+     const verticalJoin = vertical.map((q) => q.join(""));
+     for (w of verticalJoin) {
+      if (w.includes(word)) return true;
+     };
+    
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
+  
+  //pair programmed by @nicholasburgess17 & nimacortez
